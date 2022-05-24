661f56b73363c6c354bd86434f5d6401d3632570
~mipsun-diflex <ed@edwardflex.info>
2022-05-24 13:37:10 -0500

    add ipfs link to deploy code

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,5 +3,5 @@ const SonoraMaker = artifacts.require("../contracts/SonoraMaker.sol");
 
 module.exports = function (deployer) {
   //deployer.deploy(MiladyMaker);
-  deployer.deploy(SonoraMaker, 'http://localhost:8000');
+  deployer.deploy(SonoraMaker, 'https://gateway.pinata.cloud/ipfs/QmX89dvzA3TSwsGfY7SthYkDxSFjszec8JkEEZE7JP5QHF/');
 };
